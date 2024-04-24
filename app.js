2a2403a70e34f1d7971b2a6023354171e2784752
Caio Santos <caiu3835@gmail.com>
2024-04-24 14:04:23 -0300

    style: alteracao da funcao limparCampo e Adicao de Validacao de Commit

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
-  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do número secreto");
-  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 100");
 }
 
 exibirMensagemInicial();
@@ -53,8 +52,8 @@ function gerarNumeroAleatorio() {
 }
 
 function limparCampo() {
-  chute = document.querySelector("input");
-  chute.value = "";
+  let campoInput = document.querySelector("input");
+  campoInput.value = "";
 }
 
 function reiniciarJogo() {
