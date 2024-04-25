8392c4a49e2df02661adb4251747272523879329
Caio Santos <caiu3835@gmail.com>
2024-04-25 00:39:18 -0300

    build: Change in lint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    ignorePatterns: ["reiniciarJogo()", "verificarChute()"]
+  },
   pluginJs.configs.recommended,
 ];
